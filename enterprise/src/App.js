b8415392cc0b7a1b222c075579ee39be43a19e95
GnouhPTV <nhocphuontk3@gmail.com>
2023-03-13 06:55:16 +0700

    Done QandA Page

--- enterprise/src/App.js
+++ enterprise/src/App.js
@@ -1,14 +1,25 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "remixicon/fonts/remixicon.css";
+import React from 'react';
 import "./App.css";
-import Layout from "./components/Layouts/Layout";
+
+import{
+  BrowserRouter  as Router, Switch, Route, Redirect
+} from 'react-router-dom';
+import Main from './components/QandA/Main';
+import Question from './components/QandA/Add-Question/Question';
+import ViewQuestion from './components/QandA/ViewQuestion'
 
 function App() {
   return (
     <div className="App">
-      <Layout />
-      {/* <CRUD/> */}
-      {/* <LoginForm /> */}
+      <Router>
+        
+        <Switch>
+          <Route exact path='/add-question' component = {Question} />
+          <Route exact path='/question' component = {ViewQuestion} />
+          <Route exact path='/' component = {Main} />
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
