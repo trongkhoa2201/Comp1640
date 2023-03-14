cdf29c403fc20d8264593e6a5b28b496017a879f
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2023-03-14 18:31:42 +0700

    fix-bug/hoa

--- enterprise/src/App.js
+++ enterprise/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import "./App.css";
+import Layout from '../src/components/Layouts/Layout'
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import{
-  BrowserRouter  as Router, Switch, Route, Redirect
-} from 'react-router-dom';
-import Main from './components/QandA/Main';
-import Question from './components/QandA/Add-Question/Question';
-import ViewQuestion from './components/QandA/ViewQuestion'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        
-        <Switch>
-          <Route exact path='/add-question' component = {Question} />
-          <Route exact path='/question' component = {ViewQuestion} />
-          <Route exact path='/' component = {Main} />
-          
-        </Switch>
-      </Router>
+      <Layout/>
     </div>
   );
 }
--- enterprise/src/Router/Routers.js
+++ enterprise/src/Router/Routers.js
@@ -4,16 +4,26 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import CRUD from "../Page/CRUD.jsx";
 import Home from "../Page/Home.jsx";
 import Profile from "../Page/Profile.jsx";
+import Login from '../Page/Login'
+import Signup from '../Page/Signup'
+import Main from '../components/QandA/Main/Main';
+import Question from "../components/QandA/Add-Question/Question.js";
+
+
  
 
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/home" element={<Home />} />
       <Route path="/crud" element={<CRUD />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />}/>     
+      <Route path='/question' element={<Main/>}/>
+      <Route path='/add-question' element={<Question/>}/>
     </Routes>
   );
 };
