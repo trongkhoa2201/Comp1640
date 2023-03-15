f6716aaae86d53f6379bb62decb81f0aed4811ff
ntbchung2401 <85856695+ntbchung2401@users.noreply.github.com>
2023-03-15 21:29:09 +0700

    fix-change-profile

--- enterprise/src/App.js
+++ enterprise/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
-import Layout from '../src/components/Layouts/Layout'
-import "bootstrap/dist/css/bootstrap.min.css"
-
+import Layout from "../src/components/Layouts/Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "remixicon/fonts/remixicon.css";
 
 function App() {
   return (
-    <div className="App">
-      <Layout/>
+    <div className='App'>
+      <Layout />
     </div>
   );
 }
