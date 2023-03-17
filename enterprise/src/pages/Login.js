e52f1c41c74d36bc45ce55827ca01272e1c76f32
trongkhoa2201 <84830390+trongkhoa2201@users.noreply.github.com>
2023-03-18 00:24:19 +0700

    test navigate/khoa

--- enterprise/src/pages/Login.js
+++ enterprise/src/pages/Login.js
@@ -1,40 +1,57 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, FormGroup, Label, Input}from 'reactstrap';
-
+import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion'
-import '../styles/login.css'
+import '../Styles/login.css'
 
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate()
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (email === 'admin@gmail.com' && password === 'letmein') {
+      setIsLoggedIn(true);
+    }
+  }
+
+  if (isLoggedIn) {
+    navigate('/home')
+  }
   return (
     <div>
-      <Form className="form" onSubmit={(e) => this.submitForm(e)}>
+      <Form className="form" onSubmit={handleSubmit}>
         <h2 className='text-center'>Login</h2>
         <FormGroup>
           <Label>Email</Label>
           <Input
             type="email"
             name="email"
-            id="exampleEmail"
-            placeholder="Enter your email"
-
-            onChange={(e) => {
-              this.validateEmail(e);
-              this.handleChange(e);
-            }}
+            value={email}
+            onChange={handleEmailChange}
           />
         </FormGroup>
         <FormGroup>
-          <Label for="examplePassword">Password</Label>
+          <Label>Password</Label>
           <Input
             type="password"
             name="password"
-            id="examplePassword"
-            placeholder="Enter your password"
-            onChange={(e) => this.handleChange(e)}
+            value={password}
+            onChange={handlePasswordChange}
           />
         </FormGroup> <br/>
-        <motion.button whileTap={{scale:1.2}} className='login-btn'>Login</motion.button>
+        <motion.button type="submit" whileTap={{scale:1.2}} className='login-btn'>Login</motion.button>
         <p>Don't have an account? Login</p>
       </Form>
     </div>
