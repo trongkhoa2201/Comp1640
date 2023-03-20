cc158dbd98957d6c8c41dee756cbdeff3146b608
duyanhlap <duyanhlap@DESKTOP-B4SS77O>
2023-03-20 15:23:50 +0700

    Up Model

---
+++ Backend/Model/departmentModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+const DepartmentSchema = new Schema({
+    department_name: String
+})
+
+module.exports = mongoose.model('department', DepartmentSchema);
