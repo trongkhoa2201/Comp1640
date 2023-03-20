069b9046d63866b99fd8149ab1ba4df30bf51a4b
duong <duongpddgcd191250@fpt.edu.vn>
2023-03-20 15:40:22 +0700

    CRUD-Manager/duong

--- enterprise/src/Router/Routers.js
+++ enterprise/src/Router/Routers.js
@@ -1,29 +1,26 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
+import Question from "../components/QandA/Add-Question/Question.js";
+import Main from "../components/QandA/Main/Main";
 import CRUD from "../pages/CRUD";
 import Home from "../pages/Home.jsx";
+import Login from "../pages/Login";
 import Profile from "../pages/Profile.jsx";
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
-import Main from '../components/QandA/Main/Main';
-import Question from "../components/QandA/Add-Question/Question.js";
-
-
- 
-
+import Signup from "../pages/Signup";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/crud" element={<CRUD />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/login" element={<Login />}/>
-      <Route path="/signup" element={<Signup />}/>     
-      <Route path='/question' element={<Main/>}/>
-      <Route path='/add-question' element={<Question/>}/>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/question" element={<Main />} />
+      <Route path="/add-question" element={<Question />} />
     </Routes>
   );
 };
